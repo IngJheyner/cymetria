e02b72bb364eae74aebe17e4cadc96988c66d667
Leonardo Ramírez <juanlov4321@gmail.com>
2025-12-22 00:42:33 -0500

    refactor: remove GreetingService and related implementations
    - Deleted the GreetingService interface and its implementation.
    - Removed HelloController and its associated route.
    - Cleaned up the Sequelize database models directory.

--- src/domain/ports/GreetingService.ts
+++
@@ -1,3 +0,0 @@
-export interface GreetingService {
-  getGreeting(): string;
-}
--- src/infrastructure/http/services/GreetingServiceImpl.ts
+++
@@ -1,7 +0,0 @@
-import { GreetingService } from "../../../domain/ports/GreetingService";
-
-export class GreetinServiceImpl implements GreetingService {
-  getGreeting(): string {
-    return "Hola mundo";
-  }
-}
